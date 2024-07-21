4656455193e61dc781919f3d59eb61a232633315
mojtaba <mojtaba.mohamed187@gmail.com>
2024-07-22 00:35:52 +0300

    fix linting

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """
-This module contains a function to rotate a 2D matrix 90 degrees clockwise in-place.
+This module contains a function to rotate a 2D matrix 90 degrees
+clockwise in-place.
 """
+
 
 def rotate_2d_matrix(matrix):
     """
@@ -19,7 +21,7 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
